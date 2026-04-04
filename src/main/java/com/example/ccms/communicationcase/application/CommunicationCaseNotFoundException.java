93c214bd41ce41cec4de7a4dc1059526d76379b9
Volkov Sergei <volkovself@gmail.com>
2026-04-04 22:17:17 +0300

    chore(quality): add PMD static analysis

--- src/main/java/com/example/ccms/communicationcase/application/CommunicationCaseNotFoundException.java
+++ src/main/java/com/example/ccms/communicationcase/application/CommunicationCaseNotFoundException.java
@@ -1,6 +1,11 @@
 package com.example.ccms.communicationcase.application;
 
+import java.io.Serial;
+
 public class CommunicationCaseNotFoundException extends RuntimeException {
+
+  @Serial
+  private static final long serialVersionUID = 1L;
 
   public CommunicationCaseNotFoundException(String message) {
     super(message);
